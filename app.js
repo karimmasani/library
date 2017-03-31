e296f311f27305ece0ad01214fd3f064210ebbcc
karim.masani <karim.masani@outlook.com>
2017-03-31 15:23:48 -0400

    commit

--- app.js
+++ app.js
@@ -8,14 +8,25 @@ app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-    res.render('index');
+app.get('/', function(req, res) {
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [{
+                Link: '/Books',
+                Text: 'Books'
+            },
+            {
+                Link: '/Authors',
+                Text: 'Authors'
+            }
+        ]
+    });
 });
 
-app.get('/books', function (req, res) {
+app.get('/books', function(req, res) {
     res.send('Hello Books!!');
 });
 
-app.listen(port, function (err) {
+app.listen(port, function(err) {
     console.log('running server on port ' + port);
 });
